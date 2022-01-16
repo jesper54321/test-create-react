8a5afbbe9ddc19fbcf9e6671412ee7a983666b0b
Hydrille <03rytter@gmail.com>
2022-01-16 20:06:00 +0100

    update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./test.module.scss";
 
 export const Main = (props) => {
-	const [paragraphs, setParagraphs] = useState(props.paragraphs);
-	if (!paragraphs) myFetch(setParagraphs, paragraphs);
+	const [paragraphs, setParagraphs] = useState([]);
+
+	useEffect(() => {
+		myFetch();
+	}, []);
+
+	const myFetch = async () => {
+		const promise = await fetch("http://metaphorpsum.com/paragraphs/5/25");
+		let data = await promise.text();
+		data = data.split(/\n\s*\n/);
+		setParagraphs(data);
+	};
 
 	return (
-		<main>
+		<main className={styles.testclass}>
 			{paragraphs
 				? paragraphs.map((item, index) => {
 						return <p key={index}>{item}</p>;
@@ -13,11 +24,4 @@ export const Main = (props) => {
 				: ""}
 		</main>
 	);
-};
-
-const myFetch = async (setParagraphs, paragraphs) => {
-	const promise = await fetch("http://metaphorpsum.com/paragraphs/5/25");
-	let data = await promise.text();
-	data = data.split(/\n\s*\n/);
-	setParagraphs((paragraphs = data));
 };
